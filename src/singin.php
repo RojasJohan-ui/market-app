6fa5dad51536f8c25a652449836767d6763bf974
Johan Rojas <rojasbastidasjohansebastian@gmail.com>
2025-09-18 11:22:56 -0500

    cracion singin php

---
+++ src/singin.php
@@ -0,0 +1,27 @@
+<?php
+ //step 1. get data base connection
+     require('../config/database.php');
+
+     //step2. get forms database 
+     $e_mail    = $_POST['email'];
+     $p_wd      = $_POST['password'];
+
+     //step3. query to validate data
+     $sql_check_user = "
+        select 
+    	u.email,
+	    u.password
+    from 
+    	users u
+    where	
+	    u.email ='$e_mail' and 
+	    u.password ='$p_wd'
+    limit 1	 ";
+
+//step 4 execute query
+     $res_check =  pg_query($connection, $sql_check_user);
+     if (pg_num_rows($res_check) > 0){
+          echo"User exist. Go to main page!!!";
+     }else{
+            echo "Verify Data";
+     }     
--- src/singnup.php
+++ src/singnup.php
@@ -1,58 +1,57 @@
 <?php
- //step 1. get data base connection
- require('../config/database.php');
- //step2. get forms database 
- $f_name    = $_POST['fname'];
- $l_name    = $_POST['lname'];
- $m_number  = $_POST['mnumber'];
- $id_number = $_POST['idnumber'];
- $e_mail    = $_POST['email'];
- $p_wd      = $_POST['password'];
+     //step 1. get data base connection
+     require('../config/database.php');
+     //step2. get forms database 
+     $f_name    = $_POST['fname'];
+     $l_name    = $_POST['lname'];
+     $m_number  = $_POST['mnumber'];
+     $id_number = $_POST['idnumber'];
+     $e_mail    = $_POST['email'];
+     $p_wd      = $_POST['password'];
 
 
- $enc_pass = password_hash($p_wd, PASSWORD_DEFAULT);
+     $enc_pass = password_hash($p_wd, PASSWORD_DEFAULT);
 
- $check_email = "
-    SELECT
-         u.email
-    FROM 
-         users u    
-    WHERE 
-         email = '$e_mail' 
-     LIMIT 1
- ";
- $res_check =  pg_query($connection, $check_email);
-if (pg_num_rows($res_check)>0){
-   echo"<script>alert('User already exits !!')</script>";
-   hearder ('refresh:0;url=singup.html');
-} else {
- //step 3create query to insert into
- $query="
- INSERT INTO users (
-       firstname,
-       lastname,
-       mobile_number,
-       id_number,
-       email,
-       password
-  ) VALUES (
-       '$f_name', '$l_name', '$m_number', '$id_number', '$e_mail', '$p_wd'
-  )
- ";
+     $check_email = "
+     SELECT
+          u.email
+     FROM 
+          users u    
+     WHERE 
+          email = '$e_mail' 
+          LIMIT 1
+     ";
 
- //step 4 Execute query
- $res = pg_query($connection, $query);
- //step 5 valide result
- if($res){
-    //echo "users has been created successfuly !!!";
-    echo "<script>alert('Success !!! Go to login')</script>";+
-    header ('refresh:0;url=singup.html');
- }else {
-    echo "something wrong";
- }
+     $res_check =  pg_query($connection, $check_email);
 
-}
+     if (pg_num_rows($res_check) > 0){
+          echo"<script>alert('User already exits !!')</script>";
+          header('refresh:0;url=singup.html');
+     } else {
+          //step 3create query to insert into
+          $query="
+          INSERT INTO users (
+               firstname,
+               lastname,
+               mobile_number,
+               id_number,
+               email,
+               password
+          ) VALUES (
+               '$f_name', '$l_name', '$m_number', '$id_number', '$e_mail', '$p_wd'
+          )";
 
+          //step 4 Execute query
+          $res = pg_query($connection, $query);
+          //step 5 valide result
+          if($res){
+               //echo "users has been created successfuly !!!";
+               echo "<script>alert('Success !!! Go to login')</script>";+
+               header('refresh:0;url=singup.html');
+          }else {
+               echo "something wrong";
+          }
+     }
  ?>
  
  
